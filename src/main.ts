c4b101602a7f2c140da2d3790bf5afbb5e712dcb
Andrea Minetti <andrea.minetti@wavein.ch>
2019-12-09 14:02:46 +0100

    get rid of router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata' // Import only once
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import './ioc'
@@ -9,7 +8,6 @@ import './ioc'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
   vuetify,
   render: h => h(App)
