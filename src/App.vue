28f88f814802d6a3f91fa88b166d9a76827a709a
Matteo Amerena <amerenamatteo@gmail.com>
2019-12-11 16:37:44 +0100

    #3 All components defined graphically.

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,6 @@
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app clipped>
       <SampleLoader />
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app clipped-left>
