36f0529df482ef5a1b18db71ba278b0937ee408d
Andrea Minetti <andrea.minetti@wavein.ch>
2020-04-29 12:07:22 +0200

    solved conflicts

--- src/services/spectral-extractor/essentia-spectral-extractor.ts
+++ src/services/spectral-extractor/essentia-spectral-extractor.ts
@@ -9,13 +9,8 @@ export class EssentiaSpectralExtractor implements SpectralExtractor {
   analyze (sample: Float32Array): Promise<SpectralAnalisis> {
     return new Promise((resolve, reject) => {
       const worker = new Worker()
-<<<<<<< HEAD
-      let id = this.setID()
-      let msg:EssentiaMessage = new EssentiaMessage(id, 'harmony', Array.from(sample))
-=======
       let msg:EssentiaMessage = new EssentiaMessage(EssentiaMessage.HARMONY, Array.from(sample))
       console.log(msg)
->>>>>>> d39e078e1ca958b45e75d8e9a23b806db97206f6
       worker.postMessage(msg)
       worker.onmessage = (event:MessageEvent) => {
         if (msg.isForMe(event.data)) {
@@ -34,10 +29,6 @@ export class EssentiaSpectralExtractor implements SpectralExtractor {
       }
     })
   }
-<<<<<<< HEAD
-  setID ():string {
-    return '_' + Math.random().toString(36).substr(2, 9)
-  }
 
   normalize (data:number[]):Float32Array {
     let max = Math.max.apply(data)
@@ -46,6 +37,4 @@ export class EssentiaSpectralExtractor implements SpectralExtractor {
 
     return Float32Array.from(data)
   }
-=======
->>>>>>> d39e078e1ca958b45e75d8e9a23b806db97206f6
 }
