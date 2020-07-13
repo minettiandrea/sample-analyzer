fe693400d9eef7fcfb983d8eae146b75b2ef8251
Andrea Minetti <andrea.minetti@wavein.ch>
2020-07-13 18:48:32 +0200

    Voicing draft

--- src/components/harmony/VoicingsPresenter.vue
+++ src/components/harmony/VoicingsPresenter.vue
@@ -4,7 +4,12 @@
       <v-icon large left></v-icon>
       <span class="title font-weight-light">Suggested voicing</span>
     </v-card-title>
-    <v-card-text>Suggested voicing for specific sound loaded</v-card-text>
+    <v-card-text>
+      Suggested voicing for specific sound loaded
+      <ul>
+        <li v-for="(voicing,i) in voicings" :key="i">{{voicing.group}} - {{voicing.name}} - {{voicing.chord}}</li>
+      </ul>
+    </v-card-text>
   </v-card>
 </template>
 
@@ -12,25 +17,51 @@
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import { inject } from 'inversify-props'
+import { REGISTRY } from '@/ioc/registry'
+import { Store } from '@/services/store/store'
+
+export interface VoicingDefinition{
+  group:string
+  chord: number[]
+  name: string
+}
 
 @Component
 export default class VoicingsPresenter extends Vue {
-  private min7:number[][] = [[0, 3, 10], // 1 - b3 - b7 (no fifth)
-    [-9, -2, 0], // 1st inversion (b3 as bass)
-    [-5, 0, 3, 10], // 2nd inversion (5th as bass)
-    [-2, 0, 3] // 3rd inversion (7th as bass)
+
+  @inject(REGISTRY.Store) store:Store
+
+  public voicings:VoicingDefinition[] = []
+
+  private definitions = [
+    {group: "min", chord: [0, 3, 10], name: "1 - b3 - b7 (no fifth)"},
+    {group: "min", chord: [-9, -2, 0], name: "1st inversion (b3 as bass)"},
+    {group: "min", chord: [-5, 0, 3, 10], name: "2nd inversion (5th as bass)"},
+    {group: "min", chord: [-2, 0, 3], name: "3rd inversion (7th as bass)"},
+    {group: "maj", chord: [0, 4, 10], name: "1 - 3 - 7 (no fifth)"},
+    {group: "maj", chord: [-8, -1, 0], name: "1st inversion (3 as bass)"},
+    {group: "maj", chord: [-5, 0, 4, 11], name: " 2nd inversion (5th as bass)"},
+    {group: "maj", chord: [-1, 0, 4], name: "3rd inversion (7th as bass)"},
+    {group: "dom", chord: [0, 4, 10], name: "1 - 3 - b7 (no fifth)"},
+    {group: "dom", chord: [-8, -2, 0], name: "1st inversion (3 as bass)"},
+    {group: "dom", chord: [-5, 0, 4, 10], name: "2nd inversion (5th as bass)"},
+    {group: "dom", chord: [-2, 0, 4], name: "3rd inversion (7th as bass)"},
   ]
 
-    private maj7:number[][] = [[0, 4, 10], // 1 - 3 - 7 (no fifth)
-      [-8, -1, 0], // 1st inversion (3 as bass)
-      [-5, 0, 4, 11], // 2nd inversion (5th as bass)
-      [-1, 0, 4] // 3rd inversion (7th as bass)
-    ]
 
-      private dom7:number[][] = [[0, 4, 10], // 1 - 3 - b7 (no fifth)
-        [-8, -2, 0], // 1st inversion (3 as bass)
-        [-5, 0, 4, 10], // 2nd inversion (5th as bass)
-        [-2, 0, 4] // 3rd inversion (7th as bass)
-      ]
+
+  mounted() {
+    this.store.getHCPC().subscribe(hcpc => {
+      if(hcpc) {
+        const foundamental = hcpc.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0) //find the max index
+        const voicings = this.definitions.map(x => {
+          return {chord: x, score: x.chord.map(y => hcpc[(y+12+foundamental) % 12] ).reduce((x,y) => x + y)}
+        }).sort((x,y) =>  y.score - x.score).slice(0,3)
+        console.log(voicings)
+        this.voicings = voicings.map(x => x.chord)
+      }
+    })
+  }
+
 }
 </script>
