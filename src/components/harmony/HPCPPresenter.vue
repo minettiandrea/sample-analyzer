c09ab568ddd49e492e64e5e59bb2e29179778040
Andrea Minetti <andrea.minetti@wavein.ch>
2020-07-13 18:07:36 +0200

    Fix chart height, update

--- src/components/harmony/HPCPPresenter.vue
+++ src/components/harmony/HPCPPresenter.vue
@@ -4,7 +4,9 @@
 
     <v-card class="mx-auto col-10 mb-5 center">
       <h2>HPCP</h2>
-      <canvas height="285" ref='chart' class ='chart'/>
+      <div class="chart">
+        <canvas height="285" ref='chart' class ='chart'/>
+      </div>
     </v-card>
   </v-row>
 </v-container>
@@ -23,38 +25,50 @@ import { Chart } from 'chart.js'
 @Component
 export default class HPCPPresenter extends Vue  {
 
+    private chart:Chart
+
     @Ref('chart') readonly canvasChart!: HTMLCanvasElement
 
     @inject(REGISTRY.Store) store:Store
 
     mounted() {
-        this.store.getHCPC().subscribe(hcpc => {
 
         var ctx = this.canvasChart.getContext('2d');
-            if(ctx) {
-                var myChart = new Chart(ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
-                        datasets: [{
-                            label: 'Note',
-                            data: hcpc || [],
-                            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                            borderColor: 'rgba(54, 162, 235, 1)',
-                            borderWidth: 1
+        if(ctx) {
+            this.chart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
+                    datasets: [{
+                        label: 'Note',
+                        data: [],
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
                         }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }]
-                        }
                     }
-                });
+                }
+            });
+        }
+
+        this.store.getHCPC().subscribe(hcpc => {
+            if(this.chart) {
+                if(this.chart.data.datasets) {
+                    this.chart.data.datasets.forEach((dataset:any) => {
+                        dataset.data = hcpc || [];
+                    });
+                    this.chart.update();
+                }
             }
+
 
         })
     }
@@ -65,5 +79,6 @@ export default class HPCPPresenter extends Vue  {
 <style lang="scss" scoped>
 .chart{
     width:100%;
+    height: 285px;
 }
 </style>
